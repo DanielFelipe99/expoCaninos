c26e4180d1b5a6f629315f04b3f3f1dad5a84aa5
DESKTOP-4LH2CPD\usuario <davidancizar9@gmail.com>
2023-09-27 10:47:32 -0600

    Metodos, iconos en la tabla, js

--- src/main/java/com/mycompany/expocaninos/ExposicionPerro.java
+++ src/main/java/com/mycompany/expocaninos/ExposicionPerro.java
@@ -9,5 +9,52 @@ package com.mycompany.expocaninos;
  * @author danie
  */
 public class ExposicionPerro {
+    public ExposicionPerro(){
+        
+    }
+    public void ordenarPorRaza(){
+        
+    }
+    public void ordenarPorNombre(){
+        
+    }
+    public void ordenarPorPuntos(){
+        
+    }
+    public void ordenarPorEdad(){
+        
+    }
+    public int buscarPerro(){
+        
+        return 0;
+    }
+    public int buscarBinarioPorNombre(String Nombre){
+        
+        return 0;
+    }
+    public boolean agregarPerro(String nombre, String raza, String imagen, int puntos, int edad){
+        
+       
+        return false; 
+    }
+    public void verificarInvariante(){
+        
+    }
+    public boolean buscarPerrosConNombresRepetidos(){
+     
+        return false;
+    }
+    public int buscarPerroMayorPuntaje(){
+        
+        return 0;
+    }
+    public int buscarPerroMenorPuntaje(){
+        
+        return 0;
+    }
+    public int buscarPerrorMayorEdad(){
+        
+        return 1;
+    }
     
 }
